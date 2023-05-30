1f2084507323ff8bcd3cb3f7adedf8b0662cd1f5
Samuel Thompson <thomps9012@gmail.com>
2023-05-30 15:57:35 -0400

    updated pwa icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -9,76 +8,7 @@ export const metadata = {
   description:
     "An online tool to generate client satisfaction survey invitations",
 };
-const DefaultHeader = () => (
-  <Head>
-    <link rel="icon" href="/nora_favicon.png" />
-    <meta name="application-name" content="Client Survey Invitation" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-    <meta
-      name="apple-mobile-web-app-title"
-      content="Client Survey Invitation"
-    />
-    <meta
-      name="description"
-      content="An online tool to generate client satisfaction survey invitations"
-    />
-    <meta name="format-detection" content="telephone=no" />
-    <meta name="mobile-web-app-capable" content="yes" />
-    <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-    <meta name="msapplication-TileColor" content="#5D4E60" />
-    <meta name="msapplication-tap-highlight" content="no" />
-    <meta name="theme-color" content="#5D4E60" />
 
-    <link rel="apple-touch-icon" href="/client_survey_qr_code.png" />
-    <link
-      rel="apple-touch-icon"
-      sizes="152x152"
-      href="/client_survey_qr_code.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/client_survey_qr_code-retina.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="167x167"
-      href="/client_survey_qr_code.png"
-    />
-
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/client_survey_qr_code.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/client_survey_qr_code.png"
-    />
-    <link rel="manifest" href="/manifest.json" />
-    <link rel="mask-icon" href="/client_survey_qr_code.png" color="#5D4E60" />
-    <link rel="shortcut icon" href="/client_survey_qr_code.png" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Client Survey Invitation" />
-    <meta
-      property="og:description"
-      content="An online tool to generate client satisfaction survey invitations"
-    />
-    <meta property="og:site_name" content="Client Survey Invitation" />
-    <meta
-      property="og:url"
-      content="https://client-survey-invite-generator.vercel.app"
-    />
-    <meta
-      property="og:image"
-      content="https://client-survey-invite-generator.vercel.app/client_survey_qr_code.png"
-    />
-  </Head>
-);
 export default function RootLayout({
   children,
 }: {
@@ -86,7 +16,76 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <DefaultHeader />
+      <head>
+        <link rel="icon" href="/pwa_icon.png" />
+        <meta name="application-name" content="Client Survey Invitation" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Client Survey Invitation"
+        />
+        <meta
+          name="description"
+          content="An online tool to generate client satisfaction survey invitations"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#5D4E60" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#5D4E60" />
+        <link rel="apple-touch-icon" href="/pwa_icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/pwa_icon.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/pwa_icon-retina.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/pwa_icon.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/pwa_icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/pwa_icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/pwa_icon.png"
+          color="#5D4E60"
+        />
+        <link rel="shortcut icon" href="/pwa_icon.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Client Survey Invitation" />
+        <meta
+          property="og:description"
+          content="An online tool to generate client satisfaction survey invitations"
+        />
+        <meta property="og:site_name" content="Client Survey Invitation" />
+        <meta
+          property="og:url"
+          content="https://client-survey-invite-generator.vercel.app"
+        />
+        <meta
+          property="og:image"
+          content="https://client-survey-invite-generator.vercel.app/pwa_icon.png"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
